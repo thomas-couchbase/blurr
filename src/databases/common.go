e7163737f312261379eb8c19757c90465abf2297
Pavel.Paulau <pavel.paulau@gmail.com>
2012-09-30 13:25:16 -0700

    add Database interface

---
+++ src/databases/common.go
@@ -0,0 +1,27 @@
+package databases
+
+
+type Config struct {
+	Name string
+	Table string
+	Addresses []string
+	Username string
+	Password string
+}
+
+
+type Database interface {
+	Init(config Config)
+
+	Shutdown()
+
+	Create(key string, value map[string]interface{}) error
+
+	Read(key string) error
+
+	Update(key string, value map[string]interface{}) error
+
+	Delete(key string) error
+
+	Query(key string, value map[string]interface{}) error
+}
