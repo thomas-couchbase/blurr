1b39dcb66f5b85f15bcf1fbb28c4946e9b4aa296
Pavel.Paulau <pavel.paulau@gmail.com>
2012-10-06 11:27:56 -0700

    get rid of copyright notices

--- workloads/common.go
+++ workloads/common.go
@@ -1,19 +1,3 @@
-/*
- Copyright 2012 Pavel Paulau <Pavel.Paulau@gmail.com>
- All Rights Reserved
-
- Licensed under the Apache License, Version 2.0 (the "License")
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
 package workloads
 
 import (
@@ -23,7 +7,6 @@ import (
 
 	"github.com/pavel-paulau/blurr/databases"
 )
-
 
 // General workload configuration
 type Config struct {
@@ -40,7 +23,6 @@ type Config struct {
 	Workers int
 	TargetThroughput int
 }
-
 
 type Workload interface {
 	Init(config Config)
